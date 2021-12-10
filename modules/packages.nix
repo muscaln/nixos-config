e592f468167149b397223667509c9055e915f945
Mustafa Çalışkan <musfay@protonmail.com>
2021-12-10 13:50:55 +0000

    config: add theme packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -12,7 +12,9 @@
     alacritty
 
     xfce.xfce4-whiskermenu-plugin
-  
+    matcha-gtk-theme
+    papirus-icon-theme
+
     (kodi.passthru.withPackages (kodiPkgs: [kodiPkgs.pvr-iptvsimple]))
   ];
 }
