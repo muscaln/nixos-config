4e6616f0bb97d5afccbe8940fa5c31a799bc5bc6
Mustafa Çalışkan <muscaln@protonmail.com>
2021-12-27 00:55:36 +0300

    home: enable vscodium

--- home/default.nix
+++ home/default.nix
@@ -16,6 +16,21 @@
   ]; 
 
   programs = {
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = (with pkgs.vscode-extensions; [
+        bbenoist.nix
+        arrterian.nix-env-selector
+        ms-python.python
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+        name = "vscode-language-pack-tr";
+        publisher = "MS-CEINTL";
+        version = pkgs.vscodium.version;
+        sha256 = "sha256-gh6CJ+56fijEVryLWp2b8OjkF6T6whT334VXXK3+G6M=";
+      }]);
+    };
+
     neovim = {
       enable = true;
       viAlias = true;
