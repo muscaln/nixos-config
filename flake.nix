3b151d558d6811e7d46bc0e08e93d1716a4f5f69
Mustafa Çalışkan <muscaln@protonmail.com>
2021-12-26 22:06:02 +0300

    flake: add flake-utils

--- flake.nix
+++ flake.nix
@@ -3,45 +3,52 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   
-  outputs = inputs@{ self, home-manager, nixpkgs,  ... }:
-    let
-      system = "x86_64-linux";
-      username = "musfay";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+  outputs = inputs@{ self, home-manager, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        username = "musfay";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       
-     mkComputer = configurationNix: extraModules: homeModules: inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit system inputs pkgs ; };
-        modules = [
-          configurationNix
+        devshell = pkgs.mkShell {
+          buildInputs = [ scripts ];
+        };
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.musfay = {
-              programs.home-manager.enable = true;
-            } // { imports = [ ./home ] ++ homeModules; };
-          }
-        ] ++ extraModules;
-      };
-    in
+        mkComputer = configurationNix: extraModules: homeModules: nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit system inputs pkgs; };
+          modules = [
+            configurationNix
 
-    {
-      nixosConfigurations.g5070 = mkComputer
-        ./configuration.nix
-        [
-          ./modules/xfce.nix
-          ./modules/services.nix
-          ./modules/hardware-configuration.nix
-          ./modules/packages.nix
-        ]
-        [
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.musfay = {
+                programs.home-manager.enable = true;
+              } // { imports = [ ./home ] ++ homeModules; };
+            }
+          ] ++ extraModules;
+        };
+      in
+
+      {
+        nixosConfigurations.g5070 = mkComputer
+          ./configuration.nix
+          [
+            ./modules/xfce.nix
+            ./modules/services.nix
+            ./modules/hardware-configuration.nix
+            ./modules/packages.nix
+          ]
+          [
           ./home/git.nix
-        ];
-    };
+          ];
+        
+        devShell = devshell;
+      })
 }
