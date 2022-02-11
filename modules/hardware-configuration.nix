97a1b8786bc310f28faf3141205e256a29cd7db5
Mustafa Çalışkan <muscaln@protonmail.com>
2022-02-11 20:10:17 +0300

    modules: disable dhcp

--- modules/hardware-configuration.nix
+++ modules/hardware-configuration.nix
@@ -27,12 +27,5 @@
     [ { device = "/dev/disk/by-uuid/b078bc5f-8383-4e44-abd2-f64b11d3df63"; }
     ];
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = lib.mkDefault false;
-  networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
-
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
