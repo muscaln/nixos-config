34f7f1f98a7bb050ed51759851730b8d0aad7772
Mustafa Çalışkan <muscaln@protonmail.com>
2022-04-05 23:36:53 +0300

    plasma: fix browser integration (should be pushed into nixpkgs)

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -1,5 +1,12 @@
-{
+{ pkgs, ...}:
+
+let
+  jsonPath = "chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+in {
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.enable = true;
+
+  # make browser integration work
+  environment.etc."${jsonPath}".source = "${pkgs.libsForQt5.plasma-browser-integration}/etc/${jsonPath}";
 }
