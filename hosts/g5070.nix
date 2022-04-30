dae04aa3c92497249a8e4a3c26c936ae554f94dd
Mustafa Çalışkan <muscaln@protonmail.com>
2022-04-30 11:27:49 +0300

    hosts: fix partitions

--- hosts/g5070.nix
+++ hosts/g5070.nix
@@ -21,22 +21,13 @@
     "nomce"
     "nowatchdog"
     "quiet"
-    "loglevel=2"
+    "loglevel=1"
     "amdgpu.si_support=1"
     "radeon.si_support=0"
     "modules_blacklist=iTCO_wdt"
     "i915.fastboot=1"
     "mitigations=off"
   ];
-
-  environment.etc."X11/xorg.conf.d/20-intel.conf".text = ''
-Section "Device"
-    Identifier "Intel Graphics"
-    Driver "i915"
-    Option "DRI" "3"
-    Option "TearFree" "true"
-EndSection
-  '';
 
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.powersave = false;
@@ -52,15 +43,15 @@ EndSection
 
   fileSystems = {
     "/" = { 
-      device = "/dev/disk/by-uuid/71c9e1f0-0f14-480a-83c3-995f7b12c621";
+      device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
       options = [ "noatime" "nodiratime" "discard" ];
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/E2CE-535E";
+      device = "/dev/disk/by-label/EFI";
       fsType = "vfat";
     };
   };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/38fb965e-95c1-491a-8398-7b5312d9888a"; } ];
+  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 }
