4d6da95cf21d5c809663ef6d83550b23cb942f4f
Mustafa Çalışkan <muscaln@protonmail.com>
2022-04-30 11:48:41 +0300

    hosts: format

--- hosts/g5070.nix
+++ hosts/g5070.nix
@@ -1,45 +1,64 @@
 { pkgs, lib, config, modulesPath, ... }:
 
 {
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot = {
+    loader.efi.canTouchEfiVariables = true;
+    loader.grub = {
     enable = true;
     efiSupport = true;
     device = "nodev";
+    };
+
+    kernelPackages = pkgs.linuxPackages_xanmod;
+
+    extraModprobeConfig = ''
+      options rtl8723be fwlps=0 ant_sel=2
+    '';
+
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ehci_pci"
+      "ahci"
+      "usb_storage"
+      "sd_mod"
+      "sr_mod"
+      "rtsx_usb_sdmmc"
+    ];
+
+    kernelModules = [ "kvm-intel" ];
+
+    kernelParams = [
+      "nomce"
+      "nowatchdog"
+      "quiet"
+      "loglevel=1"
+      "amdgpu.si_support=1"
+      "radeon.si_support=0"
+      "modules_blacklist=iTCO_wdt"
+      "i915.fastboot=1"
+      "mitigations=off"
+    ];
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-
-  boot.extraModprobeConfig = ''
-    options rtl8723be fwlps=0 ant_sel=2
+  services.xserver.extraConfig = ''
+    Section "Device"
+      Identifier "Intel Graphics"
+      Driver "intel"
+      Option "TearFree" "true"
+    EndSection
   '';
+
+  services.xserver.videoDrivers = [
+    "intel"
+    "amdgpu"
+  ];
 
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
- 
-  boot.kernelParams = [
-    "nomce"
-    "nowatchdog"
-    "quiet"
-    "loglevel=1"
-    "amdgpu.si_support=1"
-    "radeon.si_support=0"
-    "modules_blacklist=iTCO_wdt"
-    "i915.fastboot=1"
-    "mitigations=off"
-  ];
-
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.powersave = false;
-
-  # hardware configuration
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix") ];
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
 
   fileSystems = {
     "/" = { 
