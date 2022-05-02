baf7ade5c5afa94e0e067f5ed08e6b4c060c3f04
Mustafa Çalışkan <muscaln@protonmail.com>
2022-05-02 20:27:05 +0300

    scripts: fix rebuildSystem and remove updateHardwareConfig

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,33 +1,18 @@
 { pkgs, device, self }:
 
 let
-  updateHardwareConfig = pkgs.writeScriptBin "updateHardwareConfig" ''
-    #!${pkgs.runtimeShell}
-
-    if [ [$(${pkgs.git}/bin/git diff --stat) != "" ]]; then
-      echo "Tree is dirty. Aborting."
-    else
-      export root=/
-      [ -d "/mnt/boot" ] && export root=/mnt
-      ${pkgs.nixos-install-tools}/bin/nixos-generate-config \
-        --root $root
-      ${pkgs.coreutils}/bin/mv $root/etc/nixos/hardware-configuration.nix \
-        modules/hardware-configuration.nix
-      ${pkgs.git}/bin/git add modules/hardware-configuration.nix
-      ${pkgs.git}/bin/git commit -m "updateHardwareConfig: changes"
-    fi
-  '';
-
   rebuildSystem = pkgs.writeScriptBin "rebuildSystem" ''
     #!${pkgs.runtimeShell}
+
     echo 'Rebuilding config "${device}" ...'
-    sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake $HOME/.nixos#${device}
+
+    sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake \
+    $(${pkgs.nixUnstable}/bin/nix flake archive $HOME/.nixos --json | ${pkgs.jq}/bin/jq -r .path)#${device}
   '';
 
 in pkgs.symlinkJoin {
   name = "scripts";
   paths = [
-    updateHardwareConfig
     rebuildSystem
   ];
 }
